302c1fd956fd19a90eb7c18942368684f5c10f2b
Shree Kulkarni <shree@slanglabs.in>
2020-06-09 19:05:14 +0530

    styled homepage and CS 180 SI being displayed

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import {Text, View} from "react-native-web";
+import {Text, View, StyleSheet} from "react-native";
 
 class Homepage extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Homepage</Text>
+            <View style={styles.container}>
+                <Text style={styles.title}>CS 180 SI</Text>
             </View>
         );
     };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#282c34',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 300,
+    height: 300,
+  },
+  title: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  text: {
+    color: '#fff',
+  },
+  button: {
+    borderRadius: 3,
+    padding: 20,
+    marginVertical: 10,
+    marginTop: 10,
+    backgroundColor: '#1B95E0',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
 
 export default Homepage;
