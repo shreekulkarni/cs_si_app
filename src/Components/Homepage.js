8dbbecef1afd63a50c2cce53e814ae9d76e2c3d8
Shree Kulkarni <shree@slanglabs.in>
2020-06-09 15:50:07 +0530

    homepage printing

---
+++ src/Components/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native-web";
+
+class Homepage extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>Homepage</Text>
+            </View>
+        );
+    };
+}
+
+export default Homepage;
